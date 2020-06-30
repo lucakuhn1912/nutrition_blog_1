a91e663ac3d1e7d019ce78bb316b478b18cb20c1
Luca Kuhn <luca.kuhn@hotmail.de>
2020-06-30 17:19:42 +0100

    responsive search bar and yellow background

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   def index
     @articles = Article.all
+    if params[:search]
+      @articles = Article.search(params[:search]).order("created_at DESC")
+    else
+      @articles = Article.all.order("created_at DESC")
+    end
   end
 
   def about
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,9 @@ class Article < ActiveRecord::Base
   validates :description, presence: true
   validates :category, presence: true
   has_one_attached :image
+
+  def self.search(search)
+    where("description ILIKE ? OR title ILIKE ?", "%#{search}%","%#{search}%")
+  end
+
 end
